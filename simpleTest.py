114779118325befbab3b4229eeedccba58e9553b
Leonardo Azevedo <leonardo.azevdo@gmail.com>
2022-02-21 22:15:26 -0300

    commit back quase pronta

--- simpleTest.py
+++ simpleTest.py
@@ -14,6 +14,57 @@ from readchar import readchar
 
 import matplotlib.pyplot as mpl
 
+
+def levantarGuindaste(clientID, lancaVertical, pistao, expandir_pistao,  val):
+    newVal = val * 0.1
+    newValPistao = val * 0.05
+    newValExpandirPistao = val * 1
+    sim.simxSetJointTargetVelocity(clientID, lancaVertical, newVal, sim.simx_opmode_oneshot)
+    sim.simxSetJointTargetVelocity(clientID, pistao, newValPistao, sim.simx_opmode_oneshot)
+    sim.simxSetJointTargetVelocity(clientID, expandir_pistao, newValExpandirPistao, sim.simx_opmode_oneshot)
+    time.sleep(0.2)
+    sim.simxSetJointTargetVelocity(clientID, lancaVertical, 0, sim.simx_opmode_oneshot)
+    sim.simxSetJointTargetVelocity(clientID, pistao, 0, sim.simx_opmode_oneshot)
+    sim.simxSetJointTargetVelocity(clientID, expandir_pistao, 0, sim.simx_opmode_oneshot)
+
+
+def rotacionar(clientID, cabine_rotacao , val):
+    newVal = val * 10000000000
+    sim.simxSetJointTargetVelocity(clientID, cabine_rotacao, newVal, sim.simx_opmode_oneshot)
+    time.sleep(0.2)
+    sim.simxSetJointTargetVelocity(clientID, cabine_rotacao, 0, sim.simx_opmode_oneshot)
+
+
+def expandir_lanca(clientID, lanca1, lanca2, lanca3 , val):
+    newVal = val * 0.1
+    sim.simxSetJointTargetVelocity(clientID, lanca1, newVal, sim.simx_opmode_oneshot)
+    sim.simxSetJointTargetVelocity(clientID, lanca2, newVal, sim.simx_opmode_oneshot)
+    sim.simxSetJointTargetVelocity(clientID, lanca3, newVal, sim.simx_opmode_oneshot)
+    time.sleep(0.2)
+    sim.simxSetJointTargetVelocity(clientID, lanca1, 0, sim.simx_opmode_oneshot)
+    sim.simxSetJointTargetVelocity(clientID, lanca2, 0, sim.simx_opmode_oneshot)
+    sim.simxSetJointTargetVelocity(clientID, lanca3, 0, sim.simx_opmode_oneshot)
+
+
+
+def mover_frente(clientID, roda_esquerda, roda_direita, val):
+    newVal = val * 100
+    sim.simxSetJointTargetVelocity(clientID, roda_esquerda, newVal, sim.simx_opmode_oneshot)
+    sim.simxSetJointTargetVelocity(clientID, roda_direita, -newVal, sim.simx_opmode_oneshot)
+    time.sleep(0.2)
+    sim.simxSetJointTargetVelocity(clientID, roda_esquerda, 0, sim.simx_opmode_oneshot)
+    sim.simxSetJointTargetVelocity(clientID, roda_direita, 0, sim.simx_opmode_oneshot)
+
+def mover_tras(clientID, roda_esquerda, roda_direita, val):
+    newVal = -val * 100
+    sim.simxSetJointTargetVelocity(clientID, roda_esquerda, newVal, sim.simx_opmode_oneshot)
+    sim.simxSetJointTargetVelocity(clientID, roda_direita, -newVal, sim.simx_opmode_oneshot)
+    time.sleep(0.2)
+    sim.simxSetJointTargetVelocity(clientID, roda_esquerda, 0, sim.simx_opmode_oneshot)
+    sim.simxSetJointTargetVelocity(clientID, roda_direita, 0, sim.simx_opmode_oneshot)
+
+
+
 try:
     import sim
 except:
@@ -27,46 +78,57 @@ except:
 
 import time as time
 
-# tecla = readchar()
-
-def MovimentarGuindaste(clientID, eixoTraseiroEsquerdo, eixoTraseiroDireito, val):
-    newVal = val * 0.1
-    sim.simxSetJointTargetVelocity(clientID, eixoTraseiroEsquerdo, newVal, sim.simx_opmode_oneshot)
-    sim.simxSetJointTargetVelocity(clientID, eixoTraseiroDireito, -newVal, sim.simx_opmode_oneshot)
-    time.sleep(0.2)
-    sim.simxSetJointTargetVelocity(clientID, eixoTraseiroEsquerdo, 0, sim.simx_opmode_oneshot)
-    sim.simxSetJointTargetVelocity(clientID, eixoTraseiroDireito, 0, sim.simx_opmode_oneshot)
-
-
 print('Program started')
 sim.simxFinish(-1)  # just in case, close all opened connections
-clientID = sim.simxStart('127.0.0.1', 8080, True, True, 5000, 5)  # Connect to CoppeliaSim
+clientID = sim.simxStart('25.54.18.181', 8080, True, True, 5000, 5)  # Connect to CoppeliaSim
 if clientID != -1:
     print('Connected to remote API server')
 
-    # Get Crane Handles
-    eixoTraseiroEsquerdo = sim.simxGetObjectHandle(clientID, 'RodaEixo6', sim.simx_opmode_blocking)[-1]
-    eixoTraseiroDireito = sim.simxGetObjectHandle(clientID, 'RodaEixo1', sim.simx_opmode_blocking)[-1]
+    pistao = sim.simxGetObjectHandle(clientID, 'Pistao_joint0', sim.simx_opmode_blocking)[-1]
+    lancaVerticalHandle = sim.simxGetObjectHandle(clientID, 'Lanca_joint0', sim.simx_opmode_blocking)[-1]
+    roda_esquerda_handle = sim.simxGetObjectHandle(clientID, 'RodaEsquerda', sim.simx_opmode_blocking)[-1]
+    roda_direita_handle = sim.simxGetObjectHandle(clientID, 'RodaDireita', sim.simx_opmode_blocking)[-1]
+    expandir_pistao_handle = sim.simxGetObjectHandle(clientID, 'AmpliarPistaoi_joint0', sim.simx_opmode_blocking)[-1]
+    cabine_rotacao_handle = sim.simxGetObjectHandle(clientID, 'Cabine_joint', sim.simx_opmode_blocking)[-1]
+    lanca1 = sim.simxGetObjectHandle(clientID, 'Lanca1_joint', sim.simx_opmode_blocking)[-1]
+    lanca2 = sim.simxGetObjectHandle(clientID, 'Lanca2_joint', sim.simx_opmode_blocking)[-1]
+    lanca3 = sim.simxGetObjectHandle(clientID, 'Lanca3_joint', sim.simx_opmode_blocking)[-1]
+
+    wheel_radius = 0.03
+    max_speed = 3
+    max_turn = 0.3
+    speed = 0
+    turn = 0
+    b = 0.0565
 
     # Enable imshow
     mpl.ion()
 
-    try:
-        while (1):
-            command = input('Command: ')
+    while 1:
+        command = input('Command: ')
 
-            # Move foward
-            if command == 'w':
-                MovimentarGuindaste(clientID, eixoTraseiroEsquerdo, eixoTraseiroDireito, 1)
-            # Move back
-            if command == 's':
-                MovimentarGuindaste(clientID, eixoTraseiroEsquerdo, eixoTraseiroDireito, -1)
-
-
-    except KeyboardInterrupt:
-        sim.simxFinish(clientID)
-        sys.exit()
-
+        if command == 'o':
+            levantarGuindaste(clientID, pistao, lancaVerticalHandle, expandir_pistao_handle, 35)
+        if command == 'w':
+            mover_frente(clientID, roda_esquerda_handle, roda_direita_handle, 100000)
+        if command == 'd':
+            rotacionar(clientID, cabine_rotacao_handle, -90000000)
+        if command == 'e':
+            expandir_lanca(clientID, lanca1, lanca2, lanca3, 30)
+        # while (1):
+        #     command = input('Command: ')
+        #
+        #
+        #     # Move foward
+        #     if command == 'w':
+        #     # Move back
+        #     # if command == 's':
+        #     #     MovimentarGuindaste(clientID, cabine, -1)
 else:
     print('Failed connecting to remote API server')
 print('Program ended')
+
+
+
+
+
